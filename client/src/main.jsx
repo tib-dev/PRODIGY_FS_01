6ca468236d3f1c45e77e409ab3beccd8adc22216
amiweb-7 <132199829+tib-dev@users.noreply.github.com>
2025-03-06 11:57:05 +0300

    registration update

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import Layout from "./Layout/Layout.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    
     <BrowserRouter>
       <Layout>
         <App />
